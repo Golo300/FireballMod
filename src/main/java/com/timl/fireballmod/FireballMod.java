b9a4e984a0b6a7156876dccfca7bce0e9571eceb
Tim Lanzinger <Tim.lanzinger@gmx.de>
2026-02-14 01:31:00 +0100

    add release pipeline

--- src/main/java/com/timl/fireballmod/FireballMod.java
+++ src/main/java/com/timl/fireballmod/FireballMod.java
@@ -16,7 +16,7 @@ public class FireballMod {
 
     public static final String MODID = "fireballmod";
     public static final String NAME = "Fireball Tank Mod";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.1.0";
 
     @Mod.Instance
     public static FireballMod instance;
